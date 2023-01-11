323204a6eed48ae20bb0b95ca27dadd81afcc49b
bbautista73 <bbautista73@gmail.com>
2023-01-11 00:02:35 -0500

    jumbotron almost complete

--- resources/scroll.js
+++ resources/scroll.js
@@ -1,10 +1,31 @@
-var bar = document.getElementsByClassName('btn-scroll_bar_content')
-
-var tween = TweenMax.to(bar, 0.85, {
-  height: '100%',
-  opacity: .95,
-  yoyo: true,
-  repeat: -1,
-  repeatDelay: 0.15,
-  ease: Power1.easeOut,
-})
+$(function() {
+  
+  var link = $('#navbar a.dot');
+  
+  link.on('click', function(e) {
+    var target = $($(this).attr('href'));
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 600);
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+  
+  $(window).on('scroll', function(){
+    scrNav();
+  });
+  
+  function scrNav() {
+    var sTop = $(window).scrollTop();
+    $('section').each(function() {
+      var id = $(this).attr('id'),
+          offset = $(this).offset().top-1,
+          height = $(this).height();
+      if(sTop >= offset && sTop < offset + height) {
+        link.removeClass('active');
+        $('#navbar').find('[data-scroll="' + id + '"]').addClass('active');
+      }
+    });
+  }
+  scrNav();
+});
