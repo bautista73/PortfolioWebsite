1b5667cccea3ffad40d85dafc1437a2e9cd0a6a3
bbautista73 <bbautista73@gmail.com>
2023-01-24 23:51:16 -0500

    finished responsive nav + styling

--- WebsiteDraft-Three/resources/index.js
+++ WebsiteDraft-Three/resources/index.js
@@ -44,12 +44,3 @@ function toggleMenu (event) {
 }
 
 menu.addEventListener('click', toggleMenu, false);
-
-// function myFunction() {
-//   var x = document.getElementById("navigation");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
